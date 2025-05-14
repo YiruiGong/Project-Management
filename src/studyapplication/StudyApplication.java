899422e7dc00e898c6a488a5177621508fcae54a
ThFil1696 <ThFil1696@10.16.22.157>
2025-05-14 11:27:19 -0400

    ggggdffgf

--- src/studyapplication/StudyApplication.java
+++ src/studyapplication/StudyApplication.java
@@ -10,7 +10,7 @@ package studyapplication;
  */
 public class StudyApplication {
     
-    //sahadad gong
+    //sahadad gong ewaz
     
     public static void main(String[] args) {
         // TODO code application logic here
